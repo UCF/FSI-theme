9ce6797d1df21970ba16c758e0c0c0f38ce85040
Jo Greybill <jogreybill@gmail.com>
2012-04-23 14:47:41 -0400

    Editing single.php to match two col template

--- single.php
+++ single.php
@@ -1,18 +1,43 @@
 <?php disallow_direct_load('single.php');?>
 <?php get_header(); the_post();?>
-	
-	<div class="span-24 last page-content" id="<?=$post->post_name?>">
-		<div class="span-18">
-			<article>
-				<h1><?php the_title();?></h1>
-				<?php the_content();?>
-			</article>
-		</div>
-		<div id="sidebar" class="span-6 last">
+	<div class="span4">
+        <div id="sidebar">
+			
+			<a href="<?php print site_url(); ?>"><h1><?php $site_title = get_bloginfo('name'); print $site_title; ?></h1></a>
+			
 			<?=get_sidebar();?>
+			
+			<div class="sidebar_social">
+                <a class="sidebar_socialbtn" id="sidebar_facebook" href="http://www.facebook.com/floridaspaceinstitute/">Facebook</a>
+                <a class="sidebar_socialbtn" id="sidebar_twitter" href="http://www.twitter.com/floridaspaceinstitute/">Twitter</a>
+            </div>
+            
+            <div>
+                <address>Florida Space Institute<br/>
+                    12443 Research Parkway<br/>
+                    Orlando, Florida 32333-3333<br/>
+                    407-823-0000
+                </address>
+            </div>
+			
+		</div>
+	</div>
+	
+	<div class="span8">
+			
+			<div class="contentwrap page-content" id="<?=$post->post_name?>">
+				<article>
+					<h2><?php the_title();?></h2>
+					<?php
+					if ( has_post_thumbnail() ) { 
+						the_post_thumbnail( 'page-header-img', array('class' => 'page-header-img') );
+						} ?>
+					<?php the_content();?>
+					
+				</article>
+			</div>
+			
 		</div>
 		
-		<?php get_template_part('includes/below-the-fold'); ?>
 	</div>
-
 <?php get_footer();?>
