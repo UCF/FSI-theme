9bab0e42593e51e69afad63a87ac4845ecde737a
Jo Greybill <jogreybill@gmail.com>
2012-04-24 14:32:18 -0400

    Removing get_menu_mobile(); writing this code into sidebar.php instead

--- sidebar.php
+++ sidebar.php
@@ -15,10 +15,37 @@
 <h2 class="widgettitle"><?=get_menu_title('secondary-nav-three')?></h2>
 <?=get_menu('secondary-nav-three', 'menu', 'menu-educational-partners')?>
 
-<?=get_mobile_menu('main-nav'); ?>
 
 <?php get_sidebar_extras(); ?>
 
+<div class="navbar">
+	<div class="navbar-inner">
+		<div class="container">
+
+			<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</a>
+
+			<!-- Be sure to leave the brand out there if you want it shown -->
+			<a class="brand" href="#">Navigation</a>
+
+			<!-- Everything you want hidden at 940px or less, place within here -->
+			<div class="nav-collapse">
+				<?=get_menu('main-nav', 'menu', 'menu-main-menu')?>
+				<h2 class="widgettitle"><?=get_menu_title('secondary-nav-one')?></h2>
+				<?=get_menu('secondary-nav-one', 'menu', 'menu-programs-and-projects')?>
+				<h2 class="widgettitle"><?=get_menu_title('secondary-nav-two')?></h2>
+				<?=get_menu('secondary-nav-two', 'menu', 'menu-corporate-partners')?>
+				<h2 class="widgettitle"><?=get_menu_title('secondary-nav-three')?></h2>
+				<?=get_menu('secondary-nav-three', 'menu', 'menu-educational-partners')?>
+			</div>
+
+		</div>
+	</div>
+</div>
 
 
 	</div>
