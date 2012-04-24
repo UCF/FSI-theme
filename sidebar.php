78e8d2fd8c05715b4eef92bef7c4150098c1e337
Jo Greybill <jogreybill@gmail.com>
2012-04-24 12:14:23 -0400

    Added get_sidebar_extras() and sidebar wrapper divs to sidebar.php to reduce clutter

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,12 @@
 <?php disallow_direct_load('sidebar.php');?>
 
 <?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Sidebar')):?>
+	
+	<div class="span4">
+        <div id="sidebar">
+			
+			<a href="<?php print site_url(); ?>"><h1><?php $site_title = get_bloginfo('name'); print $site_title; ?></h1></a>	
+	
 <?=get_menu('main-nav', 'menu', 'menu-main-menu')?>
 <h2 class="widgettitle"><?=get_menu_title('secondary-nav-one')?></h2>
 <?=get_menu('secondary-nav-one', 'menu', 'menu-programs-and-projects')?>
@@ -8,4 +14,10 @@
 <?=get_menu('secondary-nav-two', 'menu', 'menu-corporate-partners')?>
 <h2 class="widgettitle"><?=get_menu_title('secondary-nav-three')?></h2>
 <?=get_menu('secondary-nav-three', 'menu', 'menu-educational-partners')?>
+
+<?php get_sidebar_extras(); ?>
+
+	</div>
+</div>
+
 <?php endif;?>
