148fb5b149ccbe2ea43076169b492fc1f45a2dfd
Jo Greybill <jogreybill@gmail.com>
2012-04-24 12:03:23 -0400

    Updated to display newly-created menus instead of default wordpress sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,11 @@
 <?php disallow_direct_load('sidebar.php');?>
 
 <?php if(!function_exists('dynamic_sidebar') or !dynamic_sidebar('Sidebar')):?>
+<?=get_menu('main-nav', 'menu', 'menu-main-menu')?>
+<h2 class="widgettitle"><?=get_menu_title('secondary-nav-one')?></h2>
+<?=get_menu('secondary-nav-one', 'menu', 'menu-programs-and-projects')?>
+<h2 class="widgettitle"><?=get_menu_title('secondary-nav-two')?></h2>
+<?=get_menu('secondary-nav-two', 'menu', 'menu-corporate-partners')?>
+<h2 class="widgettitle"><?=get_menu_title('secondary-nav-three')?></h2>
+<?=get_menu('secondary-nav-three', 'menu', 'menu-educational-partners')?>
 <?php endif;?>
